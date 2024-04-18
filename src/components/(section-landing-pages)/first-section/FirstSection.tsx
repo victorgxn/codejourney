a11a766af2d754a5e94a13d394c354eb034141dc
Jaime Hermana Caffarena <jhc0001@alu.medac.es>
2024-04-18 17:39:14 +0200

    idioma cambiado

--- src/components/(section-landing-pages)/first-section/FirstSection.tsx
+++ src/components/(section-landing-pages)/first-section/FirstSection.tsx
@@ -1,10 +1,11 @@
 //TODO : Repasar el tema lazyload en los videos para mejorar el rendimiento
-
+import { SignUpButton} from '@clerk/nextjs';
 export const FirstSection = () => {
-    return (
-        <section className="md:py-20 py-10 bg-gradient-to-r from gray-00 to-gray-200 spacey-10">
-            <div className="container mx-auto text-center">
-                <div className="text-6xl flex justify-center font-bold md:px-20 pb-10
+  return (
+    <section className="md:py-20 py-10 bg-gradient-to-r from gray-00 to-gray-200 spacey-10">
+      <div className="container mx-auto text-center">
+        <div
+          className="text-6xl flex justify-center font-bold md:px-20 pb-10
             text-gradient
             bg-gradient-to-r
             from-blue-500
@@ -14,12 +15,14 @@ export const FirstSection = () => {
 
 
 
-            ">
-                    Unlock the Best in Digital Learning: Your Hub for Premier Programming Courses
+            "
+        >
+          ¡Desbloquea Tu Potencial Hoy Mismo! Tu Centro para Cursos de
+          Programación de Primera Categoría.
+        </div>
 
-                </div>
-
-                <p className="text-lg md:text-xl md-10
+        <p
+          className="text-lg md:text-xl md-10
             bg-gradient-to-r
             from-black
             to-gray-400
@@ -28,27 +31,25 @@ export const FirstSection = () => {
             font-bold
 
             "
-                >
-                    Regardless of Age or Experience, Our Platform Equips You for Success in Learning Programming
-                </p>
-                <div className="flex gap-4 justify-center pt-10">
-                    <button className="bg-blue-500 text-white px-10 py-4 rounded-md text-lg font-bold">Get Started</button>
-                    <button className="bg-gray-600 text-white px-10 py-4 rounded-md text-lg font-bold">Learn More</button>
+        >
+          Sin importar tu edad o experiencia, nuestra plataforma te prepara para
+          el éxito en el aprendizaje de programación.
+        </p>
+        <div className="flex gap-4 justify-center pt-10">
+          <SignUpButton redirectUrl="/sign-up">
+            <button className="bg-blue-500 text-white px-10 py-4 rounded-md text-lg font-bold">Empieza ahora</button>
+          </SignUpButton>
+          <SignUpButton redirectUrl="/sign-up">
+            <button className="bg-gray-600 text-white px-10 py-4 rounded-md text-lg font-bold">Saber más</button>
+          </SignUpButton>
+        </div>
 
-                </div>
-
-                <div className="pt-10">
-
-
-
-                    <video className="rounded-xl" autoPlay muted loop>
-                        <source src="/videos/hero1.mp4" type="video/mp4" />
-
-                    </video>
-
-                </div>
-
-            </div>
-
-        </section> );
-}
+        <div className="pt-10">
+          <video className="rounded-xl" autoPlay muted loop>
+            <source src="/videos/hero1.mp4" type="video/mp4" />
+          </video>
+        </div>
+      </div>
+    </section>
+  );
+};
