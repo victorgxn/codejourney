63a274a34164cf8435827ba2b3c05b80b2eed4a7
elliotmc02 <elliotmoyanocutler2002@gmail.com>
2024-04-20 20:47:53 +0200

    chatbot empezado

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Outfit } from 'next/font/google';
 import './globals.css';
 import { ClerkProvider } from '@clerk/nextjs';
 import { Toaster } from '@/components/ui/toaster';
+import Chat from '@/components/Chat/Chat';
 
 const font = Outfit({ subsets: ['latin'] });
 
@@ -23,7 +24,7 @@ export default function RootLayout({
         <body className={`${font.className} bg-gray-50 min-h-screen`}>
           {children}
           <Toaster />
-
+          <Chat />
         </body>
       </html>
     </ClerkProvider>
