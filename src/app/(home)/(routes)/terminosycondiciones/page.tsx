85266e00e9e92f5c812e0da74be219b429f920c9
Jaime Hermana Caffarena <jhc0001@alu.medac.es>
2024-04-20 16:59:49 +0200

    Politica, terminos y contacto

---
+++ src/app/(home)/(routes)/terminosycondiciones/page.tsx
@@ -0,0 +1,346 @@
+"use client";
+export default function Page() {
+  return (
+    <section className="bg-gray-50 w-full flex items-center justify-center min-h-[calc(100vh-80px)]">
+      <div className="mx-auto max-w-7xl px-6 lg:px-8">
+        <div className="py-8 px-4 mx-auto max-w-screen-xl lg:py-16 lg:px-6">
+          <div className="mx-auto max-w-screen-md"> 
+              <h1 className="text-3xl font-bold mb-4">Términos y Condiciones</h1>
+              <p className="mb-4">Última actualización: 20 de abril de 2024</p>
+              <p className="mb-4">
+                Por favor, lee estos términos y condiciones cuidadosamente antes
+                de usar Nuestro Servicio.
+              </p>
+              <h2 className="text-xl font-bold mb-2">
+                Interpretación y Definiciones
+              </h2>
+              <h3 className="font-bold mb-2">Interpretación</h3>
+              <p className="mb-4">
+                Las palabras cuya inicial está en mayúscula tienen significados
+                definidos bajo las siguientes condiciones. Las siguientes
+                definiciones tendrán el mismo significado independientemente de
+                si aparecen en singular o en plural.
+              </p>
+              <h3 className="font-bold mb-2">Definiciones</h3>
+              <p className="mb-4">
+                Para los propósitos de estos Términos y Condiciones:
+              </p>
+              <ul className="list-disc ml-8 mb-4">
+                <li>
+                  <p className="mb-2">
+                    <strong>Afiliado</strong> significa una entidad que
+                    controla, es controlada por o está bajo control común con
+                    una parte, donde "control" significa propiedad del 50% o más
+                    de las acciones, intereses patrimoniales u otros valores con
+                    derecho a voto para la elección de directores u otra
+                    autoridad administrativa.
+                  </p>
+                </li>
+                <li>
+                  <p className="mb-2">
+                    <strong>País</strong> se refiere a: España
+                  </p>
+                </li>
+                <li>
+                  <p className="mb-2">
+                    <strong>Empresa</strong> (referida como "la Empresa",
+                    "Nosotros", "Nos" o "Nuestro" en este Acuerdo) se refiere a
+                    CodeJourney, bocatas paco 123.
+                  </p>
+                </li>
+                <li>
+                  <p className="mb-2">
+                    <strong>Dispositivo</strong> significa cualquier dispositivo
+                    que pueda acceder al Servicio, como una computadora, un
+                    teléfono celular o una tableta digital.
+                  </p>
+                </li>
+                <li>
+                  <p className="mb-2">
+                    <strong>Servicio</strong> se refiere al Sitio Web.
+                  </p>
+                </li>
+                <li>
+                  <p className="mb-2">
+                    <strong>Términos y Condiciones</strong> (también referidos
+                    como "Términos") significan estos Términos y Condiciones que
+                    forman el acuerdo completo entre Tú y la Empresa con
+                    respecto al uso del Servicio. Este acuerdo de Términos y
+                    Condiciones ha sido creado con la ayuda del{" "}
+                    <a
+                      href="https://www.termsfeed.com/terms-conditions-generator/"
+                      target="_blank"
+                      className="text-blue-500"
+                    >
+                      Generador de Términos y Condiciones
+                    </a>
+                    .
+                  </p>
+                </li>
+                <li>
+                  <p className="mb-2">
+                    <strong>Servicio de Redes Sociales de Terceros</strong>{" "}
+                    significa cualquier servicio o contenido (incluidos datos,
+                    información, productos o servicios) proporcionado por un
+                    tercero que pueda ser mostrado, incluido o puesto a
+                    disposición por el Servicio.
+                  </p>
+                </li>
+                <li>
+                  <p className="mb-2">
+                    <strong>Sitio Web</strong> se refiere a CodeJourney,
+                    accesible desde{" "}
+                    <a
+                      href="codejourney.es"
+                      rel="external nofollow noopener"
+                      target="_blank"
+                      className="text-blue-500"
+                    >
+                      codejourney.es
+                    </a>
+                  </p>
+                </li>
+                <li>
+                  <p className="mb-2">
+                    <strong>Tú</strong> significa el individuo que accede o
+                    utiliza el Servicio, o la empresa u otra entidad legal en
+                    nombre de la cual dicho individuo accede o utiliza el
+                    Servicio, según corresponda.
+                  </p>
+                </li>
+              </ul>
+              <h2 className="text-xl font-bold mb-2">Reconocimiento</h2>
+              <p className="mb-4">
+                Estos son los Términos y Condiciones que rigen el uso de este
+                Servicio y el acuerdo que opera entre Tú y la Empresa. Estos
+                Términos y Condiciones establecen los derechos y obligaciones de
+                todos los usuarios con respecto al uso del Servicio.
+              </p>
+              <p className="mb-4">
+                Tu acceso y uso del Servicio está condicionado a Tu aceptación y
+                cumplimiento de estos Términos y Condiciones. Estos Términos y
+                Condiciones se aplican a todos los visitantes, usuarios y otros
+                que acceden o usan el Servicio.
+              </p>
+              <p className="mb-4">
+                Al acceder o usar el Servicio, aceptas quedar vinculado por
+                estos Términos y Condiciones. Si no estás de acuerdo con alguna
+                parte de estos Términos y Condiciones, entonces no puedes
+                acceder al Servicio.
+              </p>
+              <p className="mb-4">
+                Declaramos que tienes más de 18 años. La Empresa no permite que
+                aquellos menores de 18 años utilicen el Servicio.
+              </p>
+              <p className="mb-4">
+                Tu acceso y uso del Servicio también está condicionado a Tu
+                aceptación y cumplimiento de la Política de Privacidad de la
+                Empresa. Nuestra Política de Privacidad describe nuestras
+                políticas y procedimientos sobre la recopilación, uso y
+                divulgación de tu información personal cuando utilizas la
+                Aplicación o el Sitio Web y te informa sobre tus derechos de
+                privacidad y cómo la ley te protege. Por favor, lee nuestra
+                Política de Privacidad cuidadosamente antes de usar Nuestro
+                Servicio.
+              </p>
+              <h2 className="text-xl font-bold mb-2">Enlaces a Otros Sitios Web</h2>
+              <p className="mb-4">
+                Nuestro Servicio puede contener enlaces a sitios web o servicios
+                de terceros que no son propiedad o están bajo el control de la
+                Empresa.
+              </p>
+              <p className="mb-4">
+                La Empresa no tiene control sobre, y no asume responsabilidad
+                por el contenido, políticas de privacidad o prácticas de sitios
+                web o servicios de terceros. Además, reconoces y aceptas que la
+                Empresa no será responsable ni tendrá obligación, directa o
+                indirectamente, por cualquier daño o pérdida causados o
+                supuestamente causados por o en conexión con el uso de o la
+                confianza en el contenido, bienes o servicios disponibles en o a
+                través de dichos sitios web o servicios.
+              </p>
+              <p className="mb-4">
+                Te recomendamos encarecidamente que leas los términos y
+                condiciones y las políticas de privacidad de cualquier sitio web
+                o servicio de terceros que visites.
+              </p>
+              <h2 className="text-xl font-bold mb-2">Terminación</h2>
+              <p className="mb-4">
+                Podemos terminar o suspender Tu acceso inmediatamente, sin
+                previo aviso ni responsabilidad, por cualquier motivo, incluido,
+                entre otros, si incumples estos Términos y Condiciones.
+              </p>
+              <p className="mb-4">
+                Al terminar, Tu derecho a usar el Servicio cesará
+                inmediatamente.
+              </p>
+              <h2 className="text-xl font-bold mb-2">
+                Limitación de Responsabilidad
+              </h2>
+              <p className="mb-4">
+                No obstante cualquier daño que puedas sufrir, la responsabilidad
+                total de la Empresa y cualquiera de sus proveedores bajo
+                cualquier disposición de estos Términos y Tu recurso exclusivo
+                para todo lo anterior se limitará al monto realmente pagado por
+                Ti a través del Servicio o 100 USD si no has comprado nada a
+                través del Servicio.
+              </p>
+              <p className="mb-4">
+                En la medida máxima permitida por la ley aplicable, en ningún
+                caso la Empresa o sus proveedores serán responsables de ningún
+                daño especial, incidental, indirecto o consecuente de ninguna
+                índole (incluidos, entre otros, los daños por pérdida de
+                beneficios, pérdida de datos u otra información, interrupción
+                del negocio, lesiones personales, pérdida de privacidad derivada
+                del uso o la imposibilidad de usar el Servicio, software de
+                terceros y/o hardware de terceros utilizado con el Servicio, o
+                de otra manera en conexión con cualquier disposición de estos
+                Términos), incluso si la Empresa o cualquier proveedor ha sido
+                informado de la posibilidad de tales daños y aunque el remedio
+                falle en su propósito esencial.
+              </p>
+              <p className="mb-4">
+                Algunos estados no permiten la exclusión de garantías implícitas
+                o la limitación de responsabilidad por daños incidentales o
+                consecuentes, lo que significa que algunas de las limitaciones
+                anteriores pueden no aplicarse. En estos estados, la
+                responsabilidad de cada parte se limitará en la mayor medida
+                permitida por la ley.
+              </p>
+              <h2 className="text-xl font-bold mb-2">
+                Descargo de responsabilidad "TAL CUAL" y "SEGÚN DISPONIBILIDAD"
+              </h2>
+              <p className="mb-4">
+                El Servicio se proporciona "TAL CUAL" y "SEGÚN DISPONIBILIDAD" y
+                con todos los defectos y defectos sin garantía de ningún tipo.
+                En la medida máxima permitida por la ley aplicable, la Empresa,
+                en su propio nombre y en nombre de sus Afiliados y sus
+                respectivos licenciantes y proveedores de servicios, renuncia
+                expresamente a todas las garantías, ya sean expresas,
+                implícitas, legales o de otro tipo, con respecto al Servicio,
+                incluidas todas las garantías implícitas de comerciabilidad,
+                idoneidad para un fin particular, título y no infracción, y
+                garantías que puedan surgir de curso de trato, curso de
+                ejecución, uso o práctica comercial. Sin limitación a lo
+                anterior, la Empresa no ofrece ninguna garantía o compromiso, y
+                no realiza ninguna representación de ningún tipo de que el
+                Servicio satisfará tus requisitos, logrará resultados
+                específicos, será compatible o funcionará con cualquier otro
+                software, aplicaciones, sistemas o servicios, funcionará sin
+                interrupciones, cumplirá con cualquier estándar de rendimiento o
+                confiabilidad o será libre de errores o que cualquier error o
+                defecto se pueda corregir.
+              </p>
+              <p className="mb-4">
+                Sin limitar lo anterior, ni la Empresa ni ninguno de sus
+                proveedores realiza ninguna representación o garantía de ningún
+                tipo, expresa o implícita: (i) en cuanto al funcionamiento o
+                disponibilidad del Servicio, o la información, contenido y
+                materiales o productos incluidos en él; (ii) que el Servicio
+                será ininterrumpido o libre de errores; (iii) en cuanto a la
+                exactitud, confiabilidad o actualidad de cualquier información o
+                contenido proporcionado a través del Servicio; o (iv) que el
+                Servicio, sus servidores, el contenido o los correos
+                electrónicos enviados desde o en nombre de la Empresa están
+                libres de virus, scripts, caballos de Troya, gusanos, malware,
+                bombas de tiempo u otros componentes dañinos.
+              </p>
+              <p className="mb-4">
+                Algunas jurisdicciones no permiten la exclusión de ciertos tipos
+                de garantías o limitaciones sobre los derechos legales
+                aplicables de un consumidor, por lo que algunas o todas las
+                exclusiones y limitaciones anteriores pueden no aplicarse a Ti.
+                Pero en tal caso, las exclusiones y limitaciones establecidas en
+                esta sección se aplicarán en la mayor medida exigible por la ley
+                aplicable.
+              </p>
+              <h2 className="text-xl font-bold mb-2">Ley Aplicable</h2>
+              <p className="mb-4">
+                Las leyes del País, excluyendo sus reglas de conflicto de leyes,
+                regirán estos Términos y Tu uso del Servicio. Tu uso de la
+                Aplicación también puede estar sujeto a otras leyes locales,
+                estatales, nacionales o internacionales.
+              </p>
+              <h2 className="text-xl font-bold mb-2">Resolución de Disputas</h2>
+              <p className="mb-4">
+                Si tienes alguna preocupación o disputa sobre el Servicio,
+                aceptas intentar resolver la disputa de manera informal
+                contactando a la Empresa.
+              </p>
+              <h2 className="text-xl font-bold mb-2">
+                Para Usuarios de la Unión Europea (UE)
+              </h2>
+              <p className="mb-4">
+                Si eres un consumidor de la Unión Europea, te beneficiarás de
+                cualquier disposición obligatoria de la ley del país en el que
+                residas.
+              </p>
+              <h2 className="text-xl font-bold mb-2">
+                Conformidad Legal en los Estados Unidos
+              </h2>
+              <p className="mb-4">
+                Declaras y garantizas que (i) no te encuentras en un país que
+                esté sujeto al embargo del gobierno de los Estados Unidos, o que
+                haya sido designado por el gobierno de los Estados Unidos como
+                un país que apoya el terrorismo, y (ii) no estás incluido en
+                ninguna lista del gobierno de los Estados Unidos de partes
+                prohibidas o restringidas.
+              </p>
+              <h2 className="text-xl font-bold mb-2">Divisibilidad y Renuncia</h2>
+              <h3 className="font-bold mb-2">Divisibilidad</h3>
+              <p className="mb-4">
+                Si alguna disposición de estos Términos se considera inaplicable
+                o inválida, dicha disposición se modificará e interpretará para
+                cumplir los objetivos de dicha disposición en la mayor medida
+                posible bajo la ley aplicable y las disposiciones restantes
+                continuarán en pleno vigor y efecto.
+              </p>
+              <h3 className="font-bold mb-2">Renuncia</h3>
+              <p className="mb-4">
+                Excepto según lo establecido en este documento, el hecho de no
+                ejercer un derecho o de requerir el cumplimiento de una
+                obligación bajo estos Términos no afectará la capacidad de una
+                parte para ejercer dicho derecho o requerir dicho cumplimiento
+                en cualquier momento posterior ni constituirá una renuncia a un
+                incumplimiento posterior.
+              </p>
+              <h2 className="text-xl font-bold mb-2">
+                Interpretación de la Traducción
+              </h2>
+              <p className="mb-4">
+                Estos Términos y Condiciones pueden haber sido traducidos si los
+                hemos puesto a tu disposición en nuestro Servicio. Aceptas que
+                el texto original en inglés prevalecerá en caso de disputa.
+              </p>
+              <h2 className="text-xl font-bold mb-2">
+                Cambios en Estos Términos y Condiciones
+              </h2>
+              <p className="mb-4">
+                Nos reservamos el derecho, a nuestra entera discreción, de
+                modificar o reemplazar estos Términos en cualquier momento. Si
+                una revisión es material, haremos esfuerzos razonables para
+                proporcionar al menos 30 días de aviso antes de que entren en
+                vigencia los nuevos términos. Lo que constituye un cambio
+                material será determinado a nuestra entera discreción.
+              </p>
+              <p className="mb-4">
+                Al continuar accediendo o utilizando Nuestro Servicio después de
+                que esas revisiones entren en vigencia, aceptas quedar vinculado
+                por los términos revisados. Si no estás de acuerdo con los
+                nuevos términos, en su totalidad o en parte, por favor deja de
+                usar el sitio web y el Servicio.
+              </p>
+              <h2 className="text-xl font-bold mb-2">Contáctanos</h2>
+              <p className="mb-4">
+                Si tienes alguna pregunta sobre estos Términos y Condiciones,
+                puedes contactarnos:
+              </p>
+              <ul className="list-disc ml-8">
+                <li>Por correo electrónico: codejourneytech@gmail.com</li>
+              </ul>      
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
