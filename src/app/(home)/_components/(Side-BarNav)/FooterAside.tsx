0c5c6c57bc02685b9c7b74c923908a6c90d72847
Victor Gonzalez <134617120+victorgxn@users.noreply.github.com>
2024-04-21 13:33:02 +0200

    Boton desplegable

--- src/app/(home)/_components/(Side-BarNav)/FooterAside.tsx
+++ src/app/(home)/_components/(Side-BarNav)/FooterAside.tsx
@@ -1,9 +1,59 @@
+import {
+    SquareArrowOutUpRight,
+    Mail
+} from "lucide-react"
+
+import { Button } from "@/components/ui/button"
+import {
+    DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuGroup,
+    DropdownMenuItem,
+    DropdownMenuLabel,
+    DropdownMenuPortal,
+    DropdownMenuSeparator,
+    DropdownMenuShortcut,
+    DropdownMenuSub,
+    DropdownMenuSubContent,
+    DropdownMenuSubTrigger,
+    DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
+
 export const FooterAside = () => {
   return (
     <footer className="p-4 w-full bg-white border-t flex items-center justify-between">
       <div className="text-xs text-muted-foreground py-2 px-4 lg:flex-1">
         Copyright © 2024 codejourney.es todos los derechos reservados.
       </div>
+        <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+                <Button variant="outline">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"
+                         stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"
+                         className="lucide lucide-more-vertical text-muted-foreground h-4 w-4">
+                        <circle cx="12" cy="12" r="1"></circle>
+                        <circle cx="12" cy="5" r="1"></circle>
+                        <circle cx="12" cy="19" r="1"></circle>
+                    </svg>
+                </Button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent className="w-56">
+                <DropdownMenuGroup>
+                    <DropdownMenuItem>
+                        <Mail className="mr-2 h-4 w-4"/>
+                        <span>Contacto</span>
+                    </DropdownMenuItem>
+                    <DropdownMenuItem>
+                        < SquareArrowOutUpRight className="mr-2 h-4 w-4" />
+                        <span>Terminos y servicios</span>
+                    </DropdownMenuItem>
+                    <DropdownMenuItem>
+                        < SquareArrowOutUpRight className="mr-2 h-4 w-4" />
+                        <span>Politica de privacidad</span>
+                    </DropdownMenuItem>
+                </DropdownMenuGroup>
+            </DropdownMenuContent>
+        </DropdownMenu>
     </footer>
   );
 };
