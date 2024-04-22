867f47408ca7f37378c5c45140ed8eaff7ef64b4
elliotmc02 <elliotmoyanocutler2002@gmail.com>
2024-04-22 18:31:09 +0200

    retoques

--- src/app/api/send/route.ts
+++ src/app/api/send/route.ts
@@ -11,7 +11,7 @@ export async function POST(req: Request) {
     const { data, error } = await resend.emails.send({
       from: 'Acme <onboarding@resend.dev>',
       to: [email],
-      subject: 'Bienvenido!',
+      subject: `Bienvenido, ${userFirstName}!`,
       react: WelcomeTemplate({
         username: userFirstName,
         company: 'Codejourney',
