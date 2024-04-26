fc4af265bf90ce3a2f1a4eee6041f702e287cb4c
Jaime Hermana Caffarena <jhc0001@alu.medac.es>
2024-04-26 18:41:24 +0200

    Pagina pricing hecha y linkeada

--- src/app/(home)/(routes)/plan-premium/page.tsx
+++ src/app/(home)/(routes)/plan-premium/page.tsx
@@ -1,5 +1,286 @@
 export default function PlanPremium() {
-    return (
-        <div>Aqui va el plan premium</div>
-    );
+  return (
+    <div className="">
+      <div>
+        <h2 className="text-5xl font-bold tracki text-center mt-12 sm:text-5x1 text-sky-950">
+            Planes de precios de CodeJourney
+        </h2>
+        <p className="max-w-3xl mx-auto mt-4 text-xl text-center ">
+            Elige el plan que mejor se adapte a tus necesidades
+        </p>
+      </div>
+      <div className="mt-24 container space-y-12 lg:space-y-0 lg:grid lg:grid-cols-3 lg:gap-x-8">
+        <div className="relative p-8  border border-gray-200 rounded-2xl shadow-sm flex flex-col">
+          <div className="flex-1">
+            <h3 className="text-xl font-semibold ">Gratis</h3>
+            <p className="mt-4 flex items-baseline ">
+              <span className="text-5xl font-extrabold tracking-tight">0€</span>
+            </p>
+            <p className="mt-6 ">Solo quieres descubrir</p>
+            <ul role="list" className="mt-6 space-y-6">
+              <li className="flex">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="24"
+                  height="24"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  stroke="currentColor"
+                  strokeWidth="2"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  className="flex-shrink-0 w-6 h-6 text-blue-800"
+                  aria-hidden="true"
+                >
+                  <polyline points="20 6 9 17 4 12"></polyline>
+                </svg>
+                <span className="ml-3 ">0 CodeCoins</span>
+              </li>
+              <li className="flex">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="24"
+                  height="24"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  stroke="currentColor"
+                  strokeWidth="2"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  className="flex-shrink-0 w-6 h-6 text-blue-800"
+                  aria-hidden="true"
+                >
+                  <polyline points="20 6 9 17 4 12"></polyline>
+                </svg>
+                <span className="ml-3 ">Generar video (2 créditos)</span>
+              </li>
+              <li className="flex">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="24"
+                  height="24"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  stroke="currentColor"
+                  strokeWidth="2"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  className="flex-shrink-0 w-6 h-6 text-blue-800"
+                  aria-hidden="true"
+                >
+                  <polyline points="20 6 9 17 4 12"></polyline>
+                </svg>
+                <span className="ml-3 ">Cuestionario (1 crédito) </span>
+              </li>
+            </ul>
+          </div>
+          <a
+            className="bg-gradient-to-r
+            from-blue-900
+            to-blue-500 text-white hover:bg-emerald-100 mt-8 block w-full py-3 px-6 border-w-0 border-transparent rounded-md text-center font-medium"
+            href="/auth/login"
+          >
+            Registrarse gratis
+          </a>
+        </div>
+        <div className="relative p-8  border border-gray-200 rounded-2xl shadow-sm flex flex-col">
+          <div className="flex-1">
+            <h3 className="text-xl font-semibold ">Básico</h3>
+            <p className="mt-4 flex items-baseline ">
+              <span className="text-5xl font-extrabold tracking-tight">
+                9,99€
+              </span>
+              <span className="ml-1 text-xl font-semibold">/mes</span>
+            </p>
+            <p className="mt-6 ">Solo quieres descubrir</p>
+            <ul role="list" className="mt-6 space-y-6">
+              <li className="flex">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="24"
+                  height="24"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  stroke="currentColor"
+                  strokeWidth="2"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  className="flex-shrink-0 w-6 h-6 text-blue-800"
+                  aria-hidden="true"
+                >
+                  <polyline points="20 6 9 17 4 12"></polyline>
+                </svg>
+                <span className="ml-3 ">10 CodeCoins</span>
+              </li>
+              <li className="flex">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="24"
+                  height="24"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  stroke="currentColor"
+                  strokeWidth="2"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  className="flex-shrink-0 w-6 h-6 text-blue-800"
+                  aria-hidden="true"
+                >
+                  <polyline points="20 6 9 17 4 12"></polyline>
+                </svg>
+                <span className="ml-3 ">Generar video (2 créditos)</span>
+              </li>
+              <li className="flex">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="24"
+                  height="24"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  stroke="currentColor"
+                  strokeWidth="2"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  className="flex-shrink-0 w-6 h-6 text-blue-800"
+                  aria-hidden="true"
+                >
+                  <polyline points="20 6 9 17 4 12"></polyline>
+                </svg>
+                <span className="ml-3 ">Cuestionario (1 crédito) </span>
+              </li>
+            </ul>
+          </div>
+          <a
+            className="bg-gradient-to-r
+            from-blue-900
+            to-blue-500 text-white hover:bg-emerald-100 mt-8 block w-full py-3 px-6 border-w-0 border-transparent rounded-md text-center font-medium"
+            href="/auth/login"
+          >
+            Registrarse
+          </a>
+        </div>
+        <div className="relative p-8  border border-gray-200 rounded-2xl shadow-sm flex flex-col">
+          <div className="flex-1">
+            <h3 className="text-xl font-semibold ">Pro</h3>
+            <p className="absolute top-0 py-1.5 px-4 bg-gradient-to-r
+            from-blue-500
+            to-green-300 text-white rounded-full text-xs font-semibold uppercase tracking-wide  transform -translate-y-1/2">
+              Más popular
+            </p>
+            <p className="mt-4 flex items-baseline ">
+              <span className="text-5xl font-extrabold tracking-tight">
+                99,99€
+              </span>
+              <span className="ml-1 text-xl font-semibold">/año</span>
+            </p>
+            <p className="mt-6 ">
+              Quieres aprender y tener un asistente personal
+            </p>
+            <ul role="list" className="mt-6 space-y-6">
+              <li className="flex">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="24"
+                  height="24"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  stroke="currentColor"
+                  strokeWidth="2"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  className="flex-shrink-0 w-6 h-6 text-blue-800"
+                  aria-hidden="true"
+                >
+                  <polyline points="20 6 9 17 4 12"></polyline>
+                </svg>
+                <span className="ml-3 ">30 CodeCoins</span>
+              </li>
+              <li className="flex">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="24"
+                  height="24"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  stroke="currentColor"
+                  strokeWidth="2"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  className="flex-shrink-0 w-6 h-6 text-blue-800"
+                  aria-hidden="true"
+                >
+                  <polyline points="20 6 9 17 4 12"></polyline>
+                </svg>
+                <span className="ml-3 ">
+                  Potenciado por GPT-4 (más preciso)
+                </span>
+              </li>
+              <li className="flex">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="24"
+                  height="24"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  stroke="currentColor"
+                  strokeWidth="2"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  className="flex-shrink-0 w-6 h-6 text-blue-800"
+                  aria-hidden="true"
+                >
+                  <polyline points="20 6 9 17 4 12"></polyline>
+                </svg>
+                <span className="ml-3 ">Generar video (2 créditos)</span>
+              </li>
+              <li className="flex">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="24"
+                  height="24"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  stroke="currentColor"
+                  strokeWidth="2"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  className="flex-shrink-0 w-6 h-6 text-blue-800"
+                  aria-hidden="true"
+                >
+                  <polyline points="20 6 9 17 4 12"></polyline>
+                </svg>
+                <span className="ml-3 ">Cuestionario (1 crédito) </span>
+              </li>
+              <li className="flex">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="24"
+                  height="24"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  stroke="currentColor"
+                  strokeWidth="2"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  className="flex-shrink-0 w-6 h-6 text-blue-800"
+                  aria-hidden="true"
+                >
+                  <polyline points="20 6 9 17 4 12"></polyline>
+                </svg>
+                <span className="ml-3 ">Análisis del cuestionario</span>
+              </li>
+            </ul>
+          </div>
+          <a
+            className="bg-gradient-to-r
+            from-blue-500
+            to-green-300 text-white  mt-8 block w-full py-3 px-6 border-w-0 border-transparent rounded-md text-center font-medium"
+            href="/auth/login"
+          >
+            Registrarse
+          </a>
+        </div>
+      </div>
+    </div>
+  );
 }
--- src/app/(home)/_components/(Side-BarNav)/Aside.tsx
+++ src/app/(home)/_components/(Side-BarNav)/Aside.tsx
@@ -1,97 +1,116 @@
-'use client'
-import {Home, BookOpenCheck, Shield, MailPlus, MonitorPlay} from "lucide-react";
+"use client";
+import {
+  Home,
+  BookOpenCheck,
+  Shield,
+  MailPlus,
+  MonitorPlay,
+} from "lucide-react";
 import Link from "next/link";
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import { FooterAside } from "@/app/(home)/_components/(Side-BarNav)/FooterAside";
 
 interface MenuItem {
-    id: number;
-    name: string;
-    icon: React.ElementType; // Assuming the icon is a React component
-    path: string;
+  id: number;
+  name: string;
+  icon: React.ElementType; // Assuming the icon is a React component
+  path: string;
 }
 
 export const Aside = () => {
-    const [activeIndex, setActiveIndex] = useState(0)
-    const menuList: MenuItem[] = [
-        {
-            id: 1,
-            name: 'Cursos',
-            icon: BookOpenCheck,
-            path: '/dashboard',
-        },
-        {
-            id: 2,
-            name: 'Inicio',
-            icon: Home,
-            path: '/',
-        },
-        {
-            id: 3,
-            name: 'Plan premium',
-            icon: Shield,
-            path: '/plan-premium',
-        },
-        {
-            id: 4,
-            name: 'Mis cursos',
-            icon: MonitorPlay,
-            path: '/inscrito',
-        },
-        {
-            id: 5,
-            name: 'Promociones',
-            icon: MailPlus,
-            path: '/newsletter',
-        }
-
-    ]
-    return (
-        <aside
-            className="hidden lg:flex pt-[80px] h-full w-72 flex-col fixed inset-y-0 z-[48]"
-            aria-label="Sidebar">
-            <div className="h-full flex flex-col bg-white border-r overflow-y-auto">
-                <div className='flex flex-col w-full flex-1'>
-                    <ul className="flex flex-col w-full space-y-1.5 p-3">
-                        {menuList.map((item, index: number) => (
-                            <li key={item.id}>
-                                <Link href={item.path}
-                                    className={`flex items-center p-3 text-gray-700 rounded-lg dark:text-gray-200 hover:bg-gray-100 dark:hover:bg-gray-700 ${activeIndex === index ? 'bg-gray-100 dark:bg-gray-900' : ''}`}
-                                    onClick={() => setActiveIndex(index)}>
-                                    <item.icon className="w-6 h-6 mr-2" />
-                                    <span>{item.name}</span>
-                                    {item.id === 3 && (
-                                        <div
-                                            className="inline-flex items-center border rounded-md px-2.5 py-0.5 text-xs font-medium transition-colors focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 border-transparent bg-sky-500/10 text-sky-800 ml-auto">
-                                            Nuevo
-                                        </div>
-                                    )}
-                                </Link>
-                            </li>
-                        ))}
-                    </ul>
-                </div>
-                <div className="p-3.5">
-                    <div className="mt-52 rounded-lg border bg-card text-card-foreground shadow-none w-full">
-                        <div className="flex flex-col space-y-1.5 p-2 pt-0 md:p-4"><h3
-                            className=" flex items-center justify-center font-semibold tracking-tight text-base  bg-gradient-to-r
+  const [activeIndex, setActiveIndex] = useState(0);
+  const menuList: MenuItem[] = [
+    {
+      id: 1,
+      name: "Cursos",
+      icon: BookOpenCheck,
+      path: "/dashboard",
+    },
+    {
+      id: 2,
+      name: "Inicio",
+      icon: Home,
+      path: "/",
+    },
+    {
+      id: 3,
+      name: "Plan premium",
+      icon: Shield,
+      path: "/plan-premium",
+    },
+    {
+      id: 4,
+      name: "Mis cursos",
+      icon: MonitorPlay,
+      path: "/inscrito",
+    },
+    {
+      id: 5,
+      name: "Promociones",
+      icon: MailPlus,
+      path: "/newsletter",
+    },
+  ];
+  return (
+    <aside
+      className="hidden lg:flex pt-[80px] h-full w-72 flex-col fixed inset-y-0 z-[48]"
+      aria-label="Sidebar"
+    >
+      <div className="h-full flex flex-col bg-white border-r overflow-y-auto">
+        <div className="flex flex-col w-full flex-1">
+          <ul className="flex flex-col w-full space-y-1.5 p-3">
+            {menuList.map((item, index: number) => (
+              <li key={item.id}>
+                <Link
+                  href={item.path}
+                  className={`flex items-center p-3 text-gray-700 rounded-lg dark:text-gray-200 hover:bg-gray-100 dark:hover:bg-gray-700 ${
+                    activeIndex === index ? "bg-gray-100 dark:bg-gray-900" : ""
+                  }`}
+                  onClick={() => setActiveIndex(index)}
+                >
+                  <item.icon className="w-6 h-6 mr-2" />
+                  <span>{item.name}</span>
+                  {item.id === 3 && (
+                    <div className="inline-flex items-center border rounded-md px-2.5 py-0.5 text-xs font-medium transition-colors focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 border-transparent bg-sky-500/10 text-sky-800 ml-auto">
+                      Nuevo
+                    </div>
+                  )}
+                </Link>
+              </li>
+            ))}
+          </ul>
+        </div>
+        <div className="p-3.5">
+          <div className="mt-52 rounded-lg border bg-card text-card-foreground shadow-none w-full">
+            <div className="flex flex-col space-y-1.5 p-2 pt-0 md:p-4">
+              <h3
+                className=" flex items-center justify-center font-semibold tracking-tight text-base  bg-gradient-to-r
             from-blue-500
             to-green-300
             bg-clip-text
             text-transparent"
-                        >Pasate al plan Premium</h3><p
-                            className="text-muted-foreground text-sm ">Desbloquea todos nuestros cursos por un unico pago de 9,99 € al mes.</p></div>
-                        <div className="p-2 pt-0 md:p-4 md:pt-0"><Link
-                            className="inline-flex items-center justify-center font-medium ring-offset-background bg-gradient-to-r
+              >
+                Pasate al plan Premium
+              </h3>
+              <p className="text-muted-foreground text-sm ">
+                Desbloquea todos nuestros cursos por un unico pago de 9,99 € al
+                mes.
+              </p>
+            </div>
+            <div className="p-2 pt-0 md:p-4 md:pt-0">
+              <Link
+                className="inline-flex items-center justify-center font-medium ring-offset-background bg-gradient-to-r
             from-blue-900
             to-blue-500 transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 bg-primary text-primary-foreground hover:bg-primary/90 h-9 rounded-md px-3 w-full text-xs"
-                            href="/upgrade">Mejorar</Link></div>
-                    </div>
-                </div>
-                <FooterAside />
+                href="./plan-premium"
+              >
+                Mejorar
+              </Link>
             </div>
-        </aside>
-
-
-    );
-}
+          </div>
+        </div>
+        <FooterAside />
+      </div>
+    </aside>
+  );
+};
