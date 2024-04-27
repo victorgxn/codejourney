33a7a6ac4be14431c066a329bc46b1c443d99c6a
elliotmc02 <elliotmoyanocutler2002@gmail.com>
2024-04-27 17:35:43 +0200

    cosas

--- src/app/(home)/(routes)/inscrito/page.tsx
+++ src/app/(home)/(routes)/inscrito/page.tsx
@@ -1,113 +1,120 @@
-'use client'
+'use client';
 
-import {getEnrollCourses} from "@/app/_microservices";
-import {useUser} from "@clerk/nextjs";
-import {useEffect, useState} from "react";
-import Link from "next/link";
-import Image from "next/image";
-import {Book} from "lucide-react";
+import { getEnrollCourses } from '@/app/_microservices';
+import { useUser } from '@clerk/nextjs';
+import { useEffect, useState } from 'react';
+import Link from 'next/link';
+import Image from 'next/image';
+import { Book } from 'lucide-react';
 
 interface Course {
-    free: boolean;
-    id: string;
-    name: string;
-    description: string;
-    banner: {
-        url: string;
-    };
-    tag: string;
-    totalChapters: number;
-    author: string;
+  free: boolean;
+  id: string;
+  name: string;
+  description: string;
+  banner: {
+    url: string;
+  };
+  tag: string;
+  totalChapters: number;
+  author: string;
 }
 
 interface UserEnrollCourse {
-    courseId: string;
-    userEmail: string;
-    completedChapter: { chapterId: string }[];
-    courseList: Course[];
+  courseId: string;
+  userEmail: string;
+  completedChapter: { chapterId: string }[];
+  courseList: Course[];
 }
 
 export default function CursosInscritos() {
+  const { user } = useUser();
 
-    const {user} = useUser();
+  const [userCourseList, setUserCourseList] = useState<UserEnrollCourse[]>([]);
+  const [loading, setLoading] = useState(true); // Nuevo estado para el indicador de carga
+  useEffect(() => {
+    user ? getUserCourse() : null;
+  }, [user]);
 
-    const [userCourseList, setUserCourseList] = useState<UserEnrollCourse[]>([]);
-    const [loading, setLoading] = useState(true); // Nuevo estado para el indicador de carga
+  const getUserCourse = async () => {
+    await getEnrollCourses(user?.primaryEmailAddress?.emailAddress)
+      .then(response => {
+        console.log('userEnrollList', response);
+        if (response) {
+          setUserCourseList(response?.userEnrollCourses);
+        }
+      })
+      .finally(() => setLoading(false));
+  };
 
-    useEffect(() => {
-        user ? getUserCourse() : null;
-    }, [user]);
+  if (loading) {
+    return <div>Cargando...</div>;
+  }
 
-
-    const getUserCourse = async () => {
-        await getEnrollCourses(user?.primaryEmailAddress?.emailAddress)
-            .then(response => {
-                console.log('userEnrollList', response);
-                if (response) {
-                    setUserCourseList(response?.userEnrollCourses);
-                }
-            })
-            .finally(() => setLoading(false));
-    }
-
-    if (loading) {
-        return <div>Cargando...</div>;
-    }
-
-    return (
-        <div className="max-w-screen-xl mx-auto">
-            <div className="p-6 space-y-4 ">
-                <div className='items-center justify-center flex text-3xl font-bold md:pb-10 px-10
+  return (
+    <div className="max-w-screen-xl mx-auto">
+      <div className="p-6 space-y-4 ">
+        <div
+          className="items-center justify-center flex text-3xl font-bold md:pb-10 px-10
 bg-gradient-to-r
 from-blue-500
 to-green-300
 bg-clip-text
-text-transparent'>
-                    Bienvenido de nuevo, {user?.firstName} continua con tus cursos y mejora tus conocimientos!
-                </div>
-                <div>
-                    {userCourseList.length > 0 ? (
-                        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-5 ">
-                            {userCourseList.flatMap(enrollCourse => enrollCourse.courseList).map((course, index) => (
-                                <Link href={'/course-preview/' + course.id} key={course.id}>
-                                    <div className="border rounded-lg p-2 hover:border-blue-400 cursor-pointer">
-                                        <Image
-                                            src={course.banner.url}
-                                            alt={course.name}
-                                            width={1000}
-                                            height={500}
-                                            className="rounded-lg"
-                                            priority={false}
-                                        />
-                                        <div className="mt-1.5">
-                                            <h2 className="text-[18px] md:text-[16] font-medium ">
-                                                {course.name}
-                                            </h2>
-                                            <h2 className="text-gray-400 text-[13px]">
-                                                {course?.author ?? 'Codejourney'}
-                                            </h2>
-                                        </div>
-                                        <div className="flex items-center gap-2 mt-1.5">
-                                            <Book className="h-6 w-6 text-blue-600 rounded-full bg-blue-100 p-1" />
-                                            <h4 className="text-[12px] text-gray-400">
-                                                {course.totalChapters} Capítulos
-                                            </h4>
-                                        </div>
-                                        <h3 className="text-[14px]">…,
-                                            {course.free ? 'Gratis' : 'De pago'}
-                                        </h3>
-                                    </div>
-                                </Link>
-                            ))}
-                        </div>
-                    ) : (
-                        <div className='items-center justify-center flex text-3xl font-bold md:pb-10 px-10'>
-                            No estás inscrito en ningún curso.
-                            <Link href="/dashboard">Ver cursos disponibles</Link>
-                        </div>
-                    )}
-                </div>
-            </div>
+text-transparent"
+        >
+          Bienvenido de nuevo, {user?.firstName} continua con tus cursos y
+          mejora tus conocimientos!
         </div>
-    );
+        <div>
+          {userCourseList.length > 0 ? (
+            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-5 ">
+              {userCourseList
+                .flatMap(enrollCourse => enrollCourse.courseList)
+                .map((course, index) => (
+                  <Link href={'/course-preview/' + course?.id} key={course?.id}>
+                    <div className="border rounded-lg p-2 hover:border-blue-400 cursor-pointer">
+                      <Image
+                        src={course?.banner.url}
+                        alt={course?.name}
+                        width={1000}
+                        height={500}
+                        className="rounded-lg"
+                        priority={false}
+                      />
+                      <div className="mt-1.5">
+                        <h2 className="text-[18px] md:text-[16] font-medium ">
+                          {course?.name}
+                        </h2>
+                        <h2 className="text-gray-400 text-[13px]">
+                          {course?.author ?? 'Codejourney'}
+                        </h2>
+                      </div>
+                      <div className="flex items-center gap-2 mt-1.5">
+                        <Book className="h-6 w-6 text-blue-600 rounded-full bg-blue-100 p-1" />
+                        <h4 className="text-[12px] text-gray-400">
+                          {course?.totalChapters} Capítulos
+                        </h4>
+                      </div>
+                      <h3 className="text-[14px]">
+                        {course?.free ? 'Gratis' : 'De pago'}
+                      </h3>
+                    </div>
+                  </Link>
+                ))}
+            </div>
+          ) : (
+            <div className="items-center justify-center flex text-3xl font-bold md:pb-10 px-10">
+              No estás inscrito en ningún curso.
+              <Link
+                className="text-blue-500 hover:text-blue-300"
+                href="/dashboard"
+              >
+                Ver cursos disponibles
+              </Link>
+            </div>
+          )}
+        </div>
+      </div>
+    </div>
+  );
 }
--- src/app/_microservices/index.tsx
+++ src/app/_microservices/index.tsx
@@ -1,35 +1,41 @@
-import {gql, request} from "graphql-request";
-import {notFound} from "next/navigation";
+import { gql, request } from 'graphql-request';
+import { notFound } from 'next/navigation';
 
-const MASTER_URL = "https://api-eu-west-2.hygraph.com/v2/" + process.env.NEXT_PUBLIC_HYGRAPH_KEY + "/master";
+const MASTER_URL =
+  'https://api-eu-west-2.hygraph.com/v2/' +
+  process.env.NEXT_PUBLIC_HYGRAPH_KEY +
+  '/master';
 
-{/*Obtener toda la lista de cursos disponibles*/
+{
+  /*Obtener toda la lista de cursos disponibles*/
 }
 export const getCourseList = async () => {
-    const query = gql`
+  const query = gql`
     query courseList {
-        courseLists {
-            free
-            id
-            name
-            description
-            banner {
-                url
-            }
-            tag
-            totalChapters
-            author
+      courseLists {
+        free
+        id
+        name
+        description
+        banner {
+          url
         }
-    }`
+        tag
+        totalChapters
+        author
+      }
+    }
+  `;
 
-    return await request(MASTER_URL, query);
-}
+  return await request(MASTER_URL, query);
+};
 
-{/*Query para obtener la información de un curso por él, id*/
+{
+  /*Query para obtener la información de un curso por él, id*/
 }
 export const getCourseById = async (id: string) => {
-    try {
-        const query = gql`
+  try {
+    const query = gql`
     query getCourseById {
   courseList(where: {id: "${id}"}) {
     chapter {
@@ -50,18 +56,19 @@ export const getCourseById = async (id: string) => {
     tag
     author
   }
-}`
-        return await request(MASTER_URL, query);
-    } catch (error) {
-        notFound();
-    }
+}`;
+    return await request(MASTER_URL, query);
+  } catch (error) {
+    notFound();
+  }
+};
+
+{
+  /*Query para saber si el usuario está inscrito en el curso*/
 }
-
-
-{/*Query para saber si el usuario está inscrito en el curso*/}
 export const isUserEnrollCourse = async (id: string, userEmail: string) => {
-    try {
-        const query = gql`
+  try {
+    const query = gql`
             query isUserEnrollCourse {
                 userEnrollCourses(where: {courseId: "${id}", userEmail: "${userEmail}"}){
                     courseId
@@ -72,45 +79,53 @@ export const isUserEnrollCourse = async (id: string, userEmail: string) => {
                         }
                     }
                 }
-            }`
-        return await request(MASTER_URL, query);
-    } catch (error) {
-        notFound();
-    }
-}
+            }`;
+    return await request(MASTER_URL, query);
+  } catch (error) {
+    notFound();
+  }
+};
 
-{/*Crear el draft para que un usuario pueda inscribirse a un curso*/
+{
+  /*Crear el draft para que un usuario pueda inscribirse a un curso*/
 }
-export const EnrollCourse = async (courseId: string, userEmail: string | undefined) => {
-    const mutationQuery = gql`
+export const EnrollCourse = async (
+  courseId: string,
+  userEmail: string | undefined
+) => {
+  const mutationQuery = gql`
     mutation EnrollCourse {
-        createUserEnrollCourse(data: {userEmail: "${userEmail}", courseId: "${courseId}"}) {
+        createUserEnrollCourse(data: {userEmail: "${userEmail}", courseId: "${courseId}", courseList: {connect: {id: "${courseId}"}}}) {
         id
         }
-    }`
+    }`;
 
-    return await request(MASTER_URL, mutationQuery);
-}
+  return await request(MASTER_URL, mutationQuery);
+};
 
-{/*//TODO : Pensar mejor que estado devolver cuando se haga la peticion, facilidades futuras*/
+{
+  /*//TODO : Pensar mejor que estado devolver cuando se haga la peticion, facilidades futuras*/
 }
-{/*Publicar el draft creado anteriormente*/
+{
+  /*Publicar el draft creado anteriormente*/
 }
 export const PublishCourse = async (idDraft: string) => {
-    const mutationQuery = gql`
+  const mutationQuery = gql`
     mutation EnrollCoursePublish {
         publishUserEnrollCourse(where: {id: "${idDraft}"}) {
             id
         }
-    }`
+    }`;
 
-    return await request(MASTER_URL, mutationQuery);
+  return await request(MASTER_URL, mutationQuery);
+};
+
+{
+  /*Obtener la lista de cursos que está inscrito un usuario por el email*/
 }
 
-{/*Obtener la lista de cursos que está inscrito un usuario por el email*/}
-
 export const getEnrollCourses = async (userEmail: string | undefined) => {
-    const query = gql`
+  const query = gql`
         query getEnrollCourses {
             userEnrollCourses(where: { userEmail: "${userEmail}" }) {
                 courseList {
@@ -128,5 +143,5 @@ export const getEnrollCourses = async (userEmail: string | undefined) => {
             }
         }
     `;
-    return await request(MASTER_URL, query);
-}
+  return await request(MASTER_URL, query);
+};
