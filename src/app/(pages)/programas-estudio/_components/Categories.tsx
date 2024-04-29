c9da885da970226ce506f06f5bafa3b1fae09c4e
elliotmc02 <elliotmoyanocutler2002@gmail.com>
2024-04-29 02:27:21 +0200

    cambios extras

--- src/app/(pages)/programas-estudio/_components/Categories.tsx
+++ src/app/(pages)/programas-estudio/_components/Categories.tsx
@@ -9,11 +9,19 @@ import {
 } from 'react-icons/si';
 import { FaJava } from 'react-icons/fa';
 
-import { useCategory } from '@/context/CategoryContext';
+import clsx from 'clsx';
 
-export type Technology = 'react' | 'tailwind' | 'astro' | 'mysql' | 'laravel' | 'java' | 'nextjs'; // Exporta el tipo Technology
+export type Technology =
+  | 'react'
+  | 'tailwind'
+  | 'astro'
+  | 'mysql'
+  | 'laravel'
+  | 'java'
+  | 'nextjs'; // Exporta el tipo Technology
 
 interface CategoriesProps {
+  selectedTechnology: Technology;
   setSelectedTechnology: (technology: Technology) => void;
 }
 
@@ -22,55 +30,72 @@ interface FilterOption {
   name: string;
   value: string;
   icon: any;
+  primary: string;
+  accent: string;
 }
 
-export const Categories: React.FC<CategoriesProps> = ({ setSelectedTechnology }) => {
+export const Categories: React.FC<CategoriesProps> = ({
+  selectedTechnology,
+  setSelectedTechnology,
+}) => {
   const categories: FilterOption[] = [
     {
       id: 1,
       name: 'React',
       value: 'react',
       icon: SiReact,
+      primary: 'text-react-primary',
+      accent: 'text-react-accent',
     },
     {
       id: 2,
       name: 'Tailwind',
       value: 'tailwind',
       icon: SiTailwindcss,
+      primary: 'text-tailwind-primary',
+      accent: 'text-tailwind-accent',
     },
     {
       id: 3,
       name: 'Astro',
       value: 'astro',
       icon: SiAstro,
+      primary: 'text-astro-primary',
+      accent: 'text-astro-accent',
     },
     {
       id: 4,
       name: 'MySQL',
       value: 'mysql',
       icon: SiMysql,
+      primary: 'text-mysql-primary',
+      accent: 'text-mysql-accent',
     },
     {
       id: 5,
       name: 'Laravel',
       value: 'laravel',
       icon: SiLaravel,
+      primary: 'text-laravel-primary',
+      accent: 'text-laravel-accent',
     },
     {
       id: 6,
       name: 'Java',
       value: 'java',
       icon: FaJava,
+      primary: 'text-java-primary',
+      accent: 'text-java-accent',
     },
     {
       id: 7,
       name: 'NextJs',
       value: 'nextjs',
       icon: SiNextdotjs,
+      primary: 'text-nextjs-primary',
+      accent: 'text-nextjs-accent',
     },
   ];
-
-  const { selectedCategory, setSelectedCategory } = useCategory();
 
   return (
     <>
@@ -78,10 +103,12 @@ export const Categories: React.FC<CategoriesProps> = ({ setSelectedTechnology })
         <div
           onClick={() => setSelectedTechnology(category.value as Technology)}
           key={category.id}
-          className="m-2 text-center hover:bg-gray-200 transition duration-300 ease-in-out rounded-lg p-4 flex flex-col justify-center items-center"
+          className={`m-2 text-center hover:bg-gray-200 transition duration-300 ease-in-out rounded-lg p-4 flex flex-col justify-center items-center cursor-pointer ${
+            selectedTechnology === category.value ? 'bg-gray-300' : undefined
+          }`}
         >
-          <category.icon size="2x" className="w-10 h-10" />
-          <p>{category.name}</p>
+          <category.icon className={clsx('w-10 h-10', `${category.primary}`)} />
+          <p className="font-semibold">{category.name}</p>
         </div>
       ))}
     </>
--- src/app/(pages)/programas-estudio/page.tsx
+++ src/app/(pages)/programas-estudio/page.tsx
@@ -1,4 +1,4 @@
-"use client";
+'use client';
 import { useState } from 'react';
 import { TypewriterEffect } from '@/components/ui/typewriter-effect';
 import { Categories } from './_components/Categories';
@@ -8,13 +8,15 @@ import { Technology } from './_components/Categories'; // Importa el tipo Techno
 // Define la variable metadata dentro del componente
 const metadata = {
   title: 'Programas de Estudios | Codejourney',
-  description: 'Codejourney es tu plataforma para el aprendizaje en línea de desarrollo de software. Ofrecemos una amplia gama de cursos especializados en desarrollo de backend y frontend, diseñados para impulsar tu carrera como desarrollador.',
+  description:
+    'Codejourney es tu plataforma para el aprendizaje en línea de desarrollo de software. Ofrecemos una amplia gama de cursos especializados en desarrollo de backend y frontend, diseñados para impulsar tu carrera como desarrollador.',
 };
 
 // Define el componente ProgramaEstudio
 export default function ProgramaEstudio() {
   // Define el estado para la tecnología seleccionada
-  const [selectedTechnology, setSelectedTechnology] = useState<Technology>('react'); // Por defecto, establece la tecnología seleccionada en 'react'
+  const [selectedTechnology, setSelectedTechnology] =
+    useState<Technology>('react'); // Por defecto, establece la tecnología seleccionada en 'react'
 
   // Retorna el JSX del componente ProgramaEstudio
   return (
@@ -38,10 +40,13 @@ export default function ProgramaEstudio() {
         <div className="text-center mb-8">
           <h2 className="text-2xl font-bold mb-4">Tecnologías</h2>
           <div className="flex flex-wrap mb-8 bg-white shadow-lg rounded-lg justify-center max-w-ld mx-auto">
-            <Categories setSelectedTechnology={setSelectedTechnology} />
+            <Categories
+              selectedTechnology={selectedTechnology}
+              setSelectedTechnology={setSelectedTechnology}
+            />
           </div>
         </div>
-        <Courses technology={selectedTechnology} /> 
+        <Courses technology={selectedTechnology} />
       </div>
     </>
   );
